49ceec445041d5d09ca1c174b2e1d2bc7e1139a7
Arif <absojol@gmail.com>
2026-05-19 21:03:36 +0600

    Maximum done Alhamdulillah

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
-app.use(express.json())
-require('dotenv').config();
+app.use(express.json());
 
-//wwk1LVvGdbpuNlRS
-//ArifCivicUser
-
-const uri = `mongodb+srv://ArifCivicUser:wwk1LVvGdbpuNlRS@cluster0.6yzpwvv.mongodb.net/`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6yzpwvv.mongodb.net/`;
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -72,6 +68,21 @@ async function run() {
             res.send(result)
         });
 
+        // Latest APIs
+        app.get('/recent-issues', async (req, res) => {
+            const cursor = issuesDb.find().sort({ created_at: -1 }).limit(6);
+            const result = await cursor.toArray();
+            res.send(result)
+        });
+
+        app.get('/issues/contribute/:issueIdM', async (req, res) => {
+            const issueIdParam = req.params.issueIdM;
+            const query = { issueId: issueIdParam }
+            const cursor = issueContributions.find(query).sort({ bid_price: -1 })
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
         app.get('/issues/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
@@ -91,10 +102,7 @@ async function run() {
             const updatedIssue = req.body;
             const query = { _id: new ObjectId(id) }
             const update = {
-                $set: {
-                    title: updatedIssue.title,
-                    price: updatedIssue.price
-                }
+                $set: updatedIssue
             }
 
             const result = await issuesDb.updateOne(query, update)
@@ -105,14 +113,6 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await issuesDb.deleteOne(query);
-            res.send(result);
-        })
-
-        app.get('/issues/contribute/:issueId', async (req, res) => {
-            const issueId = req.params.issueId;
-            const query = { issue: issueId }
-            const cursor = issueContributions.find(query).sort({ bid_price: -1 })
-            const result = await cursor.toArray();
             res.send(result);
         })
 
